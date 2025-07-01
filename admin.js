1849e3822ae6d8322c6fb8cda968a902bd0433d8
w68162 <w68162@student.wsiz.edu.pl>
2025-07-01 12:09:54 +0200

    project

---
+++ admin.js
@@ -0,0 +1,6 @@
+function showSection(id) {
+    document.querySelectorAll('.section').forEach(section => {
+      section.classList.remove('active');
+    });
+    document.getElementById(id).classList.add('active');
+  }
